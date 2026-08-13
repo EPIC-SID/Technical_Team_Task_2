21a74aaa9d9f0e0a170cca162b3141058ccbe302
EPIC-SID <siddhant.verma370@gmail.com>
2026-08-13 10:42:39 +0530

    [task 1] : fixed the opp logic inside the functions and updated gitignore to ignore .exe and .o files

--- questions/question-01-student-search.cpp
+++ questions/question-01-student-search.cpp
@@ -13,12 +13,12 @@ int firstPosition(int marks[], int n, int target)
 
     while (low <= high)
     {
-        int mid = (low + high) / 2;
+        int mid = low + (high - low) / 2;
 
         if (marks[mid] == target)
         {
             answer = mid;
-            low = mid + 1;
+            high = mid - 1;
         }
         else if (marks[mid] < target)
         {
@@ -29,7 +29,7 @@ int firstPosition(int marks[], int n, int target)
             high = mid - 1;
         }
     }
-
+    
     return answer;
 }
 
@@ -41,12 +41,12 @@ int lastPosition(int marks[], int n, int target)
 
     while (low <= high)
     {
-        int mid = low + (high - low) / 2;
+        int mid = (low + high) / 2;
 
         if (marks[mid] == target)
         {
             answer = mid;
-            high = mid - 1;
+            low = mid + 1;
         }
         else if (marks[mid] < target)
         {
